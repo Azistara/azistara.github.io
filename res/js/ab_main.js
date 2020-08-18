31ab1254d633a13199a51f6abe5dde81328c7153
Azuri <47091791+Azistara@users.noreply.github.com>
2020-08-18 00:00:04 -0700

    Update index.html

---
+++ res/js/ab_main.js
@@ -0,0 +1,25 @@
+//Aiboku
+//Written August 17, 2020
+
+///GLOBAL ELEMENTS///
+//Navigation
+document.getElementById("navMnu").innerHTML =
+    '<nav id="nav" class="blue lighten-2 z-depth-3 navbar-fixed">' +
+    '<a href="#" class="brand-logo navLogo">Azistara</a>' +
+    '<ul class="right hide-on-med-and-down">' +
+    '<li><a href="https://instagram.com/Azistara/">Instagram</a></li>' +
+    '<li><a href="https://github.com/Azistara">GitHub</a></li>' +
+    '</ul>' +
+    '</nav>';
+
+//Footer
+document.getElementById("footer").innerHTML =
+    '<footer class="page-footer grey lighten-3">'+
+    '<div class="footer-copyright" style="padding: 0px 10px 0px 10px">' +
+    '<p>' +
+    '<center class="grey-text">' +
+    '©2020 Azistara.' +
+    '</center>' +
+    '</p>' +
+    '</div>'+
+    '</footer>';
